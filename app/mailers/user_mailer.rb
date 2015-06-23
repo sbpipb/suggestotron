71e04b0e7b17a7d9ea01266e10aec317b2da47ab
sbpipb <julianmacmang@gmail.com>
2015-06-23 18:54:19 +0800

    created mailer.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,21 @@
+class UserMailer < ApplicationMailer
+
+  # default from: 'no-reply@suggestotron.me'
+  # default template_path: 'def_templ'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def topic_email(topic)
+  	@topic = topic
+  	mail(to:'julian@dispostable.com',
+        subject: 'New Topic:'+@topic.title+' created',
+        template_path: 'topic_mailer',
+        template_name: 'topic_email'
+      )
+  end
+
+end
