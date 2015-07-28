1464d4ddcc5e16265d7c6d69bfbf8ac599908a2f
jvlivnmvcmvng <julianmacmang@gmail.com>
2015-07-28 11:41:56 +0800

    signout redirect path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   private
-  def after_sign_out_path_for(user)
-  	new_user_session_path
+  def after_sign_out_path_for(resource_or_scope)
+  	new_user_session_path	
   end
 
 end
