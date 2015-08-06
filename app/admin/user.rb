61fd7dae0b539b6667dfdbaf8455554a3fecc87f
jvlivnmvcmvng <julianmacmang@gmail.com>
2015-08-06 12:10:08 +0800

    admin user and user resource

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,30 +1,31 @@
 ActiveAdmin.register User do
-
-permit_params :email, :city, :address, :password
-filter :email
-  index do
-    selectable_column
-    id_column
-    column :email
-    column :city
-    column :address
-    column :created_at
-    # column :updated_at
-    column :last_sign_in_at
-    actions
-  end
-
-  form do |f|
-    f.inputs "Basic Information" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-      f.input :city
-      f.input :address      
+  menu parent: "Accounts"  
+  permit_params :email, :city, :address, :password
+  
+  filter :email
+    index do
+      selectable_column
+      id_column
+      column :email
+      column :city
+      column :address
+      column :created_at
+      # column :updated_at
+      column :last_sign_in_at
+      actions
     end
 
-    f.actions
-  end
+    form do |f|
+      f.inputs "Basic Information" do
+        f.input :email
+        f.input :password
+        f.input :password_confirmation
+        f.input :city
+        f.input :address      
+      end
+
+      f.actions
+    end
 
 
 end
